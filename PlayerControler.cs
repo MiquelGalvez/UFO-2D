675ea74684d6f3424c2af6f89505173c2c514922
MiquelGalvez <145144000+MiquelGalvez@users.noreply.github.com>
2023-09-26 18:22:49 +0200

    Add files via upload

---
+++ PlayerControler.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI text;
+    [SerializeField]
+    TextMeshProUGUI textvictoria;
+    [SerializeField]
+    float speed;
+    private Rigidbody2D rb2d;
+
+    int punt = 0;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float moveHorizontall = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontall,moveVertical);
+        rb2d.AddForce (movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Pickup")
+        { 
+            punt++;
+            text.text = ("Puntuacio: " +punt.ToString() + " | Queden: " + (4-punt) + " Pickups per recollir");
+            if (punt == 3)
+            {
+                text.text = ("Puntuacio: " + punt.ToString() + " | Queda: " + (4 - punt) + " Pickup per recollir");
+            }
+            Destroy(collision.gameObject);
+        }
+
+        if (punt == 4){
+            text.text = text.text = ("Puntuacio: " + punt.ToString());
+            textvictoria.text = ("Miquel Galvez, Has Guanyat!!!!");
+        }
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
